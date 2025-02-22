ba9c9205595d3951335a3c092745322d5505a886
alvarogtz98 <alvaro.gutierrez@uabc.edu.mx>
2025-02-21 21:42:56 -0800

    segunda version

---
+++ Downloads/Maestria MGTIC/DSAP/Desarrollo de apps/script/script.js
@@ -0,0 +1,27 @@
+// keyword variableName = value;
+// var, let, const
+
+// String
+var firstName = "Sofia";
+var lastName = "Romo";
+
+firstName = "Fernanda";
+console.log(firstName);
+
+// numerical
+let myAge = 99;
+let myHeight = 100;
+
+console.log(myAge, myHeight);
+
+// boolean (true/false)
+let isStudent = true;
+
+// math operations + - / *
+let num1 = 10;
+let num2 = 12;
+
+console.log("El resultado es = " + (num1 + num2));
+console.log("El resultado es = " + (num1 - num2));
+console.log("El resultado es = " + (num1 * num2));
+console.log("El resultado es = " + (num1 / num2).toFixed(3));
